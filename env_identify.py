213ef88e08ff0cdc72f825fb737b2cdf91da3e41
Malcolm-XY <m18307300600@gmail.com>
2024-10-03 16:32:15 +0900

    renew

--- env_identify.py
+++ env_identify.py
@@ -1,7 +1,9 @@
 import sys
 import torch
+import torchaudio
 
 # 优化：使用 f-string 写入字符串到文件
-with open("venv_test_output.txt", "a") as file:
+with open("env_identify_output.txt", "a") as file:
     file.write(f"Python interpreter: {sys.executable}\n")
     file.write(f"Torch version: {torch.__version__}\n")
+    file.write(f"Torch version: {torchaudio.__version__}\n")
