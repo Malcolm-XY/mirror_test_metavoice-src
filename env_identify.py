e59ef6afad4464b9984bdf335d18a4f8e14e3fc4
Malcolm-XY <m18307300600@gmail.com>
2024-10-03 20:31:40 +0900

    Update env_identify.py

--- env_identify.py
+++ env_identify.py
@@ -7,3 +7,8 @@ with open("env_identify_output.txt", "a") as file:
     file.write(f"python interpreter: {sys.executable}\n")
     file.write(f"torch version: {torch.__version__}\n")
     file.write(f"torchaudio version: {torchaudio.__version__}\n")
+    # 检查是否安装了 CUDA
+    if torch.cuda.is_available():
+        file.write("CUDA is available! PyTorch is using GPU.")
+    else:
+        file.write("CUDA is not available. PyTorch is using CPU.")
