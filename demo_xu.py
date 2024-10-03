593318020e17e0fede076c1c2757555f2cd3993c
Malcolm-XY <m18307300600@gmail.com>
2024-10-03 16:37:50 +0900

    renew

---
+++ demo_xu.py
@@ -0,0 +1,4 @@
+import torch；
+from fam.llm.fast_inference import TTS;
+
+#tts = TTS()
--- env_identify.py
+++ env_identify.py
@@ -4,6 +4,6 @@ import torchaudio
 
 # 优化：使用 f-string 写入字符串到文件
 with open("env_identify_output.txt", "a") as file:
-    file.write(f"Python interpreter: {sys.executable}\n")
-    file.write(f"Torch version: {torch.__version__}\n")
-    file.write(f"Torch version: {torchaudio.__version__}\n")
+    file.write(f"python interpreter: {sys.executable}\n")
+    file.write(f"torch version: {torch.__version__}\n")
+    file.write(f"torchaudio version: {torchaudio.__version__}\n")
