7aef6116e9555b109da8837618968521aed1ce57
Malcolm-XY <m18307300600@gmail.com>
2024-10-03 17:42:36 +0900

    Update demo_xu.py

--- demo_xu.py
+++ demo_xu.py
@@ -1,5 +1,5 @@
 import torch;
 import fam.llm.fast_inference as fast_infer;
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda")
 tts = fast_infer.TTS()
