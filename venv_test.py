4a9a7d9409a5b50f157689d0dd6a6313e41b379c
Malcolm-XY <m18307300600@gmail.com>
2024-10-03 16:19:16 +0900

    Update venv_test.py

--- venv_test.py
+++ venv_test.py
@@ -1,5 +1,7 @@
+import sys
 import torch
 
-# 写入字符串到文件
+# 优化：使用 f-string 写入字符串到文件
 with open("venv_test_output.txt", "a") as file:
-    file.write(torch.__version__)
+    file.write(f"Python interpreter: {sys.executable}\n")
+    file.write(f"Torch version: {torch.__version__}\n")
